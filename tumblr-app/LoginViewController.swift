953bc112e2623dbfdc56f0769f64bd66716c9f13
Idelfonso <idelfonsog2@gmail.com>
2016-10-15 16:20:18 -0400

    feat: init repo and UI

---
+++ tumblr-app/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  tumblr-app
+//
+//  Created by Idelfonso on 10/15/16.
+//  Copyright © 2016 Idelfonso. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func loginWithTumblr(_ sender: AnyObject) {
+        
+        
+    }
+
+
+}
+
---
+++ tumblr-app/TMClient.swift
@@ -0,0 +1,15 @@
+//
+//  TMClient.swift
+//  tumblr-app
+//
+//  Created by Idelfonso on 10/15/16.
+//  Copyright © 2016 Idelfonso. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    static let ConsumerKey = "Y1rZXHptROHv4rGJFGdfbPVkakE2HXPwjd3wRpnjrJeUPyRiIr"
+    static let SecretKey = "JbdFQBF6QQUILBRXhVgc2PKOhWs3mGMfi57KswESEUoC1z1Hls"
+    
+}
