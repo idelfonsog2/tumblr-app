44bdc865a53a680e58560f6fc945bab9855d978c
Idelfonso <idelfonsog2@gmail.com>
2016-10-17 18:39:52 -0400

    feat: add query parameters for tumblr

--- tumblr-app/TMConstants.swift
+++ tumblr-app/TMConstants.swift
@@ -9,16 +9,18 @@
 import UIKit
 
 struct Constants {
-    
+    //MARK: Credentials
     static let ConsumerKey = "Y1rZXHptROHv4rGJFGdfbPVkakE2HXPwjd3wRpnjrJeUPyRiIr"
-    static let ApiKey = "JbdFQBF6QQUILBRXhVgc2PKOhWs3mGMfi57KswESEUoC1z1Hls"
 
+    //MARK: Authentication
     static let RequestToken = "https://www.tumblr.com/oauth/request_token"
     static let Authorize = "https://www.tumblr.com/oauth/authorize"
     static let AccessToken = "https://www.tumblr.com/oauth/access_token"
     
+    //MARK: Request
     static let RequestURL = "https://api.tumblr.com/v2"
     
+    //MARK: URL Components
     static let scheme = "https"
     static let host = "api.tumblr.com"
     static let path = "/v2"
@@ -26,16 +28,45 @@ struct Constants {
 
 
 struct Methods {
+    //GET
     static let BlogFollowers = "/blog/{blog-identifier}/followers"
     static let Tagged = "/tagged"
     static let UserInfo = "/user/info"
     static let UserDashboard = "/user/dashboard"
     
-    static let PostText = "/blog/idelfonsog2.tumblr.com/posts/text"
+    //POST
+    static let PostText = "/blog/idelfonsog2.tumblr.com/post"
 }
 
 struct ParameterKeys {
+    //Include when required
+    static let ApiKey = "api_key"
+    
+    //Search method
     static let Tag = "tag"
+    
+    //Post blog
+    static let BlogType = "type"
+    static let BlogState = "state"
+    //Text Post
+    static let Title = "title"
+    
+}
+
+struct ParameterValues {
+    //General include when required
+    static let ApiKey = "JbdFQBF6QQUILBRXhVgc2PKOhWs3mGMfi57KswESEUoC1z1Hls"
+    
+    //Blog Post Types
+    static let TextType = "text"
+    static let PhotoType = "photo"
+    static let QuoteType = "quote"
+    static let LinkType = "link"
+    static let ChatType = "chat"
+    static let AudioType = "audio"
+    static let VideoType = "video"
+    
+    
 }
 
 
--- tumblr-app/TMEditBlogViewController.swift
+++ tumblr-app/TMEditBlogViewController.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
+import OAuthSwift
 
 class TMEditBlogViewController: UIViewController {
 
+    //MAR: Properties
+    var oauth1swift: OAuth1Swift? = nil
+    
+    //MARK: IBOutlets
+    @IBOutlet weak var textBlog: UITextView!
+    
+    //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .plain, target: self, action: #selector(TMEditBlogViewController.postTextBlog))
         // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(TMEditBlogViewController.postBlog))
+        self.oauth1swift = TMClient.sharedInstance().oauth1swift as! OAuth1Swift?
     }
     
-    func postTextBlog() {
-        print("testing")
+    //MARK: Actions
+    func postBlog() {
+        print(oauth1swift?.parameters)
+        let headers = ["Accept": "application/json", "Content-Type":"application/json"]
+        let parameters = [ParameterKeys.BlogType:ParameterValues.TextType]
+        
+        oauth1swift?.client.post(Methods.PostText, parameters: parameters , headers: headers, body: <#T##Data?#>, success: <#T##OAuthSwiftHTTPRequest.SuccessHandler?##OAuthSwiftHTTPRequest.SuccessHandler?##(Data, HTTPURLResponse) -> Void#>, failure: <#T##OAuthSwiftHTTPRequest.FailureHandler?##OAuthSwiftHTTPRequest.FailureHandler?##(OAuthSwiftError) -> Void#>)
+    }
+    
+    //MARK: Helpers
+    func displayStatus() {
+        //Status from the post update
     }
 
 
