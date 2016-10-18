032a89e96eeec62c4d2e59b165c48443b0c3bbf6
Idelfonso <idelfonsog2@gmail.com>
2016-10-18 15:54:36 -0400

    refactor:SearchViewController

--- TMSearchViewController.swift
+++ TMSearchViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import OAuthSwift
 
-class TMSearchViewController: UIViewController {
+class TMSearchViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
     var oauth1swift: OAuth1Swift? = nil
@@ -17,25 +17,35 @@ class TMSearchViewController: UIViewController {
     //MARK: IBOutlets
     
     
-    //MARK: Delegates
-    var searchTextFieldDelegate = SearchTextFieldDelegate()
-    
     //MAR: Life Cycle
     override func viewDidLoad() {
-        super.viewDidLoad()
-        self.oauth1swift = TMClient.sharedInstance().oauth1swift as! OAuth1Swift?
         
-        oauth1swift!.client.request(
-            TMClient.sharedInstance().tumblrURL(withPathExtension: Methods.UserInfo),
-            method: .GET,
-            success: {
-                data, response in
-                let json = TMClient.sharedInstance().convertToJSONObject(data: data)
-//                print(json)
-            },
-            failure: {
+        super.viewDidLoad()
+        
+        self.oauth1swift = TMClient.sharedInstance().oauth1swift as! OAuth1Swift?
+    }
+    
+    //MAR: UITextFieldDelegate
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text! as NSString
+        
+        var newText = oldText.replacingCharacters(in: range, with: string)
+        
+        print(newText)
+        
+        let parameters = [String:Any!]()
+        
+        oauth1swift?.client.request(TMClient.sharedInstance().tumblrURL(withPathExtension: Methods.PostText), method: .POST, parameters: parameters, headers: nil, success: {
+            (data, error) in
+            let json = TMClient.sharedInstance().convertToJSONObject(data: data)
+            
+            }, failure: {
                 error in
                 print(error)
         })
+        
+        return true
     }
+    
 }
