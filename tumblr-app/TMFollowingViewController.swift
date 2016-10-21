45d6a41c6d4bfef4143f1fc62b74ae569896e595
Idelfonso <idelfonsog2@gmail.com>
2016-10-21 17:37:57 -0400

    feat: show list of blogs I'm following

--- tumblr-app/TMFollowingViewController.swift
+++ tumblr-app/TMFollowingViewController.swift
@@ -13,82 +13,91 @@ class TMFollowingViewController: UIViewController, UITableViewDataSource, UITabl
 
     //MARK: Properties
     var oauth1swift: OAuth1Swift? = nil
-    var blogs: [TMBlog]?
-    var session = TMClient.sharedInstance()
-    var blogName: String?
-    
+    var blogs: [[String:AnyObject]]?
+
     //MAR: IBOutlets
-    
+    @IBOutlet weak var blogTableView: UITableView!
+
     //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.blogs = [TMBlog]()
+
+        //init OAuth
         self.oauth1swift = TMClient.sharedInstance().oauth1swift as! OAuth1Swift?
+
         self.showBlogs()
     }
-    
+
     //MARK: Func
-    
+
     func showBlogs() {
-        
-        //Query param for /user/following
-        let parameters = [
-            ParameterKeys.ApiKey: ParameterKeys.ApiKey
-        ]
-        
-        //GET request
-        let _ = oauth1swift?.client.request(session.tumblrURL(Methods.UsersFollowing), method: .GET, parameters: parameters, headers: nil, success: {
-             (data, error) in
-            
-                let json = TMClient.sharedInstance().convertToJSONObject(data)
-                print(json)
-                if let results = json["response"] as? [[String:AnyObject]] {
-                    self.blogs = TMBlog.blogsFromResults(results: results)
-                    for blog in self.blogs! {
-                        //self.blogName = blog["name"] as! String
-                    }
-                
-                }
-            
-            }, failure: {
-                (error) in
-                print("Error in GET Call request to \(Methods.UsersFollowing)")
-                print(error.localizedDescription)
-        })
-        
+
+      //Query params for /users/following
+      let parameters = [
+          ParameterKeys.ApiKey: ParameterValues.ApiKey
+      ]
+
+      //GET request
+      let _ = oauth1swift?.client.request(TMClient.sharedInstance().tumblrURL(Methods.UsersFollowing), method: .GET, parameters: parameters, headers: nil, success: {
+           (data, error) in
+
+          let json = TMClient.sharedInstance().convertToJSONObject(data)
+
+          guard let results = json["response"] as? [String:AnyObject] else {
+              print("No response key found")
+              return
+          }
+
+          guard let blogsDictionaryArray = results["blogs"] as? [[String:AnyObject]] else {
+              print("No 'blogs' key found")
+              return
+          }
+
+          self.blogs = blogsDictionaryArray
+
+          DispatchQueue.main.async {
+              self.blogTableView.reloadData()
+          }
+
+          }, failure: {
+              (error) in
+              print(error)
+      })
+
     }
-    
-    
+
+
     //MARK: UITableViewDelegate
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (self.blogs?.count)!
+        //FIX:
+        return 4
     }
-    
-    
+
+
     //Display name of blog in row text
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchViewCell", for: indexPath) as UITableViewCell!
-        
-        if let blog = self.blogs?[indexPath.row] {
-            if let name = blog.name {
-                cell?.textLabel?.text = "\(name)"
-            }
-        }
-        
-        return cell!
+      let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingViewCell", for: indexPath)
+
+      let blog = self.blogs?[indexPath.row]
+
+      if let name = blog?["name"] as! String? {
+          cell.textLabel?.text = name
+          print(name)
+      }
+
+      return cell
     }
-    
+
     //Show Details for search results
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let blog = self.blogs?[indexPath.row]
-//        
-//        let controller = storyboard!.instantiateViewController(withIdentifier: "TMDetailFollowingViewController") as! TMDetailFollowingViewController
-//        
-//        controller.blog = blog!
-//        
-//        self.present(controller, animated: true, completion: nil)
-        
+//      let blog = self.blogs?[indexPath.row]
+//
+//      let controller = storyboard?.instantiateViewController(withIdentifier: "TMUnfollowViewController") as! TMUnfollowViewController
+//
+//      //controller.blog = blog
+//
+//      self.present(controller, animated: true, completion: nil)
+
     }
 }
---
+++ tumblr-app/TMUnfollowViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TMUnfollowViewController.swift
+//  tumblr-app
+//
+//  Created by Idelfonso on 10/21/16.
+//  Copyright © 2016 Idelfonso. All rights reserved.
+//
+
+import UIKit
+
+class TMUnfollowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
